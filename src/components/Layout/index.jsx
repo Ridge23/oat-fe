519f21fd7f7bd197491a5b0d5b6630e3f8ccf136
Pavel Khrebto <pavel@zports.com>
2018-12-13 15:37:59 +0100

    Fix eslint warnings

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './assets/styles/layout.scss';
 
-function Layout({children}) {
-    return (
-        <div className="layout">
-            {children}
-        </div>
-    )
+function Layout({ children }) {
+    return <div className="layout">{children}</div>;
 }
+
+Layout.propTypes = {
+    children: PropTypes.node.isRequired
+};
 
 export default Layout;
