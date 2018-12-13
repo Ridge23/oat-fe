71dddb40258db7268ece741f5d85a8d7604cfbf8
Pavel Khrebto <pavel@zports.com>
2018-12-13 15:53:57 +0100

    Fix for tests

--- src/components/Layout/tests/Layout.test.js
+++ src/components/Layout/tests/Layout.test.js
@@ -4,6 +4,6 @@ import Layout from '../index';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Layout />, div);
+    ReactDOM.render(<Layout>Some content</Layout>, div);
     ReactDOM.unmountComponentAtNode(div);
 });
